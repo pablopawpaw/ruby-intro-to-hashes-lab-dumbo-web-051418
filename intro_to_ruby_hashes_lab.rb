a85b8de87e57cc2865aedf33263788820648c204
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-07 19:34:58 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
 	  railroads: {
 	    pieces: 4,
 	    names: {
-	      reading_railroad: {}
+	      reading_railroad: {},
 	      pennsylvania_railroad: {},
 	      b_and_o_railroad: {},
 	      shortline: {}
@@ -51,7 +51,7 @@ def monopoly_with_fourth_tier
 	    names: {
 	      reading_railroad: {
 	        "mortgage_value" => "$100"
-	      }
+	      },
 	      pennsylvania_railroad: {
 	        "mortgage_value" => "$200"
 	      },
